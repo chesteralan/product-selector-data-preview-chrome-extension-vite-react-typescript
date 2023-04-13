63b0b49c315a4a6b5d75ae7f4e8943fca0dd3b18
Alchie Tagudin <alchie.tagudin@thepetlabco.com>
2023-04-14 03:22:34 +0800

    detect cta urls

--- react-app/src/components/FloatingNav/FloatingNavBuilder.tsx
+++ react-app/src/components/FloatingNav/FloatingNavBuilder.tsx
@@ -1,4 +1,7 @@
+import { useEffect, useState } from "react";
 import EditButton from "../Buttons/EditButton";
+import Button from "../Buttons/Button";
+import CtaButton from "../Buttons/CtaButton";
 
 type Props = {
   entryId: string;
@@ -7,19 +10,31 @@ type Props = {
 const styles: React.CSSProperties = {
   position: "fixed",
   width: 30,
-  top: `50vh`,
+  top: `30vh`,
   right: 0,
   display: `block`,
   zIndex: 9999,
 };
 
 const FloatingNavBuilder = ({ entryId }: Props) => {
-
+  const [cta, setCta] = useState<string[]>([]);
+  useEffect(() => {
+    const elements = document.getElementsByTagName("a");
+    const ctas = Array.from(new Set(Array.from(elements).map((element) => {
+        if ((/^(https:\/\/offer\.thepetlabco\.com)/g).test(element.href)) {
+            return element.href
+        }
+    }).filter(Boolean)));
+    if(ctas.length > 0) {
+        setCta(ctas as string[]);
+    }
+  }, [setCta]);
   return (
     <div style={styles}>
       <EditButton funnelId={entryId} />
+      {cta.map((offerCta) => <CtaButton link={offerCta} />)}
     </div>
   );
 };
 
-export default FloatingNavBuilder
+export default FloatingNavBuilder;
