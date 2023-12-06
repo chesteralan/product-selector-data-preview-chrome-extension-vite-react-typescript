d55827ef765789559ad4d5273e39d68222a25c3b
Alchie Tagudin <alchie.tagudin@thepetlabco.com>
2023-12-07 05:23:34 +0800

    chore: added region links

--- src/components/FunnelSelector/NextJs/StrapiEditLinks/StrapiEditLinks.tsx
+++ src/components/FunnelSelector/NextJs/StrapiEditLinks/StrapiEditLinks.tsx
@@ -1,45 +1,140 @@
-import useConfig from '../../../../hooks/useConfig';
-import * as S from './StrapiEditLinks.styles'
+import useConfig from "../../../../hooks/useConfig";
+import * as S from "./StrapiEditLinks.styles";
+import { getSiteRegion } from "../../../../utils/getSiteRegion";
+import {
+  REGION_CA,
+  REGION_UK,
+  REGION_US,
+} from "../../../../utils/constants/region";
 
 type Props = {
-    data: any;
-}
+  data: any;
+};
 
 const StrapiEditLinks = ({ data }: Props) => {
-    
-    const { strapiServerUrl, stagingUrl, localUrl, liveUrl } = useConfig();
+  const {
+    strapiServerUrl,
+    stagingUrl,
+    localUrl,
+    liveUrl,
+    usStagingUrl,
+    caStagingUrl,
+    ukStagingUrl,
+  } = useConfig();
 
-    const locale = data.locale;
-    const locales = data.locales;
-    const pageId = data.props.pageProps?.page?.id;
-    const pageVariantId = data.props.pageProps?.pageVariant?.id;
-    const promoId = data.props.pageProps?.page?.promo?.id;
-    const slug = data.query?.slug || null;
-    const variant = data.query?.variant || null;
+  const locale = data.locale;
+  const locales = data.locales;
+  const pageId = data.props.pageProps?.page?.id;
+  const pageVariantId = data.props.pageProps?.pageVariant?.id;
+  const promoId = data.props.pageProps?.page?.promo?.id;
+  const slug = data.query?.slug || null;
+  const variant = data.query?.variant || null;
+
+  const region = getSiteRegion();
+  const pathname = window.location.pathname;
 
   return (
     <div style={S.Container}>
-      {strapiServerUrl && (<>
-        <a href={`${strapiServerUrl}/admin`} style={S.Link}>Admin Panel</a>
-        |
-        {pageId && (<a href={`${strapiServerUrl}/admin/content-manager/collectionType/api::page.page/${pageId}?plugins[i18n][locale]=${locale}`} style={S.Link}>Edit Page</a>)}
-        {pageVariantId && (<a href={`${strapiServerUrl}/admin/content-manager/collectionType/api::page-variant.page-variant/${pageVariantId}?plugins[i18n][locale]=${locale}`} style={S.Link}>Edit Variant</a>)}
-        {promoId && (<a href={`${strapiServerUrl}/admin/content-manager/collectionType/api::promo.promo/${promoId}?plugins[i18n][locale]=${locale}`} style={S.Link}>Edit Promo</a>)}
-        |
-        {slug && locales.map((loc:string) => loc !== locale && (<a href={`/${loc}/${slug}${variant ? `/${variant}` : ``}`} style={S.Link}>{loc}</a>))}
-        </>)}
-        |
-        {stagingUrl && slug && (<>
-          <a href={`${stagingUrl}/${slug}${variant ? `/${variant}` : ``}`} style={S.Link}>Staging</a>
-        </>)}
-        {localUrl && slug && (<>
-          <a href={`${localUrl}/${slug}${variant ? `/${variant}` : ``}`} style={S.Link}>Local</a>
-        </>)}
-        {liveUrl && slug && (<>
-          <a href={`${liveUrl}/${slug}${variant ? `/${variant}` : ``}`} style={S.Link}>Live</a>
-        </>)}
+      {region !== REGION_US && (
+        <a href={usStagingUrl as string} style={S.Link}>
+          US
+        </a>
+      )}
+      {region !== REGION_CA && (
+        <a href={caStagingUrl as string} style={S.Link}>
+          CA
+        </a>
+      )}
+      {region !== REGION_UK && (
+        <a href={ukStagingUrl as string} style={S.Link}>
+          UK
+        </a>
+      )}
+      {strapiServerUrl && (
+        <>
+          |
+          <a href={`${strapiServerUrl}/admin`} style={S.Link}>
+            Admin Panel
+          </a>
+          {pageId && (
+            <>
+              |
+              <a
+                href={`${strapiServerUrl}/admin/content-manager/collectionType/api::page.page/${pageId}?plugins[i18n][locale]=${locale}`}
+                style={S.Link}
+              >
+                Edit Page
+              </a>
+              {pageVariantId && (
+                <a
+                  href={`${strapiServerUrl}/admin/content-manager/collectionType/api::page-variant.page-variant/${pageVariantId}?plugins[i18n][locale]=${locale}`}
+                  style={S.Link}
+                >
+                  Edit Variant
+                </a>
+              )}
+              {promoId && (
+                <a
+                  href={`${strapiServerUrl}/admin/content-manager/collectionType/api::promo.promo/${promoId}?plugins[i18n][locale]=${locale}`}
+                  style={S.Link}
+                >
+                  Edit Promo
+                </a>
+              )}
+            </>
+          )}
+        </>
+      )}
+      {pathname !== "/" && (
+        <>
+          |
+          {slug &&
+            locales.map(
+              (loc: string) =>
+                loc !== locale && (
+                  <a
+                    href={`/${loc}/${slug}${variant ? `/${variant}` : ``}`}
+                    style={S.Link}
+                  >
+                    {loc}
+                  </a>
+                ),
+            )}
+          |
+          {stagingUrl && slug && (
+            <>
+              <a
+                href={`${stagingUrl}/${slug}${variant ? `/${variant}` : ``}`}
+                style={S.Link}
+              >
+                Staging
+              </a>
+            </>
+          )}
+          {localUrl && slug && (
+            <>
+              <a
+                href={`${localUrl}/${slug}${variant ? `/${variant}` : ``}`}
+                style={S.Link}
+              >
+                Local
+              </a>
+            </>
+          )}
+          {liveUrl && slug && (
+            <>
+              <a
+                href={`${liveUrl}/${slug}${variant ? `/${variant}` : ``}`}
+                style={S.Link}
+              >
+                Live
+              </a>
+            </>
+          )}
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default StrapiEditLinks
+export default StrapiEditLinks;
