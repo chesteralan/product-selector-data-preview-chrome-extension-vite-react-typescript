4a7d09e0a60b6bfd5ab0fd3e8199ed8e1db73f1a
Alchie Tagudin <chesteralantagudin@gmail.com>
2024-08-03 02:27:46 +0800

    v1.42.2: new features

--- src/components/OptionsPage/StrapiCMS/StrapiCMS.tsx
+++ src/components/OptionsPage/StrapiCMS/StrapiCMS.tsx
@@ -2,15 +2,17 @@ import Input from "../Input";
 import * as S from "../MainOptions/MainOptions.styles";
 import { REGIONS_LIST } from "../../../utils/constants/region";
 
-type Props = {};
-
-const StrapiCMS = (props: Props) => {
+const StrapiCMS = () => {
   return (
     <div style={S.Container}>
       <h2>Strapi CMS Servers</h2>
       {REGIONS_LIST.map((region) => (
         <div key={region}>
           <h3 style={S.Title}>{region}</h3>
+          <Input
+            title={`Strapi CMS Server URL - ${region}`}
+            storeKey={`${region.toLowerCase()}StrapiServerUrl`}
+          />
           <Input
             title={`Green - Strapi CMS URL - ${region}`}
             storeKey={`${region.toLowerCase()}StrapiGreenUrl`}
